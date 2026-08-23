7e5a91e9a8237db3631b4fd73d97f31c4a2daf0c
yessetassan <139701904+yessetassan@users.noreply.github.com>
2026-08-23 20:49:44 +0500

    update bot features

--- src/main/java/com/yesset/telegram_bot/session/SessionState.java
+++
@@ -1,8 +0,0 @@
-package com.yesset.telegram_bot.session;
-
-public enum SessionState {
-    CHOOSING_MODE,
-    GRAMMAR_WAITING_KAZAKH,
-    GRAMMAR_WAITING_TRANSLATION,
-    WORD_WAITING_WORD
-}
--- src/main/java/com/yesset/telegram_bot/telegram/InlineButton.java
+++
@@ -1,4 +0,0 @@
-package com.yesset.telegram_bot.telegram;
-
-public record InlineButton(String text, String callbackData) {
-}
